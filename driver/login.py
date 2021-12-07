768af0b36d575dfa2fa5a000e488eb77d5f23fe3
Bagusosh <bagusosh@gmail.com>
2021-12-07 13:21:36 +0700

    Driver : Added Login Driver

--- driver/login.py
+++ driver/login.py
@@ -20,7 +20,7 @@ class LoginDriver:
         else:
             self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROME_DRIVER"))
 
-        self.login_url = "https://silvi-staging.netlify.app"
+        self.login_url = "https://silvi-merchant-develop.netlify.app/"
         self.valid_email = 'samsantechrestoran@mailnesia.com'
         self.valid_password = '123456'
         self.valid_merchant_name = 'Samsan Tech Restoran!!'
