50ab2386810d31de2597f3bceca491bf4c9f4d25
Bagusosh <bagusosh@gmail.com>
2022-01-11 16:58:49 +0700

    Initialization : Main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from loguru import logger
 from tests.auth.login import LoginTests
 from tests.report.sales.sales_summary import SalesSummaryTests
 from tests.report.sales.payment_method import PaymentMethodTests
+from tests.report.sales.menu import SalesPerMenuTests
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
